cb9cfde044f97026e8ccc26caea6f0b2c48192b7
Jamy Golden <code@jamygolden.com>
2025-01-16 17:45:46 +0100

    Fix bug where vim doesn't send enough information to switch to tmux
    This adds pane_id info to the tmux command like vim-tmux-navigator

--- lua/nvim-tmux-navigation/tmux_util.lua
+++ lua/nvim-tmux-navigation/tmux_util.lua
@@ -7,15 +7,35 @@ local tmux_directions = { ['p'] = 'l', ['h'] = 'L', ['j'] = 'D', ['k'] = 'U', ['
 --
 -- the check if tmux is actually running (so the variable $TMUX is
 -- not nil) is made before actually calling this function
-local function tmux_command(command)
+local function tmux_command(args)
     local tmux_socket = vim.fn.split(vim.env.TMUX, ',')[1]
-    return vim.fn.system("tmux -S " .. tmux_socket .. " " .. command)
+    local command = {
+      "tmux",
+      "-S",
+      tmux_socket,
+    };
+
+    -- Concat args into command
+    for _, v in ipairs(args) do
+        table.insert(command, v)
+    end
+
+    return vim.fn.system(command)
 end
 
 -- check whether the current tmux pane is zoomed
 local function is_tmux_pane_zoomed()
-    -- the output of the tmux command is "1\n", so we have to test against that
-    return tmux_command("display-message -p '#{window_zoomed_flag}'") == "1\n"
+    local zoomed_value = tmux_command({
+        "display-message",
+        "-p",
+        "#{window_zoomed_flag}",
+    }):gsub("%s+", "") -- the output of the tmux command is "1\n", so we strip that away
+
+    if zoomed_value == "1" then
+      return true
+    end
+
+    return false
 end
 
 -- whether tmux should take control over the navigation
@@ -28,7 +48,14 @@ end
 
 -- change the current pane according to direction
 function util.tmux_change_pane(direction)
-    tmux_command("select-pane -" .. tmux_directions[direction])
+    local pane_id = vim.env.TMUX_PANE;
+
+    tmux_command({
+        "select-pane",
+        "-t",
+        pane_id,
+        "-" .. tmux_directions[direction],
+    })
 end
 
 -- capitalization util, only capitalizes the first character of the whole word
